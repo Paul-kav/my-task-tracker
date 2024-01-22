59d9c6b4f158f9bf56608b20951bb3bf79840e0e
Paul-kav <paulkavuma82@gmail.com>
2024-01-22 04:05:42 -0500

    Added default props to the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import  PropTypes from 'prop-types'
 const Button = ({color, text}) => {
   return (
     <div>
       <button className='btn' style={{backgroundColor: color}}>{text}</button>
     </div>
-  )
+    )
+}
+
+Button.defaultProps ={
+    color: 'skyblue',
+}
+
+Button.propTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
 }
 
 export default Button
